bbe44277248a23a5e4921225d03ed0f82df126da
misha1potapenko <misha1potapenko@yandex.ru>
2022-07-09 10:14:05 +0300

    added text homework4

--- C#Homework1/Homework2/Program.cs
+++ C#Homework1/Homework2/Program.cs
@@ -3,3 +3,14 @@
 //2, 3, 7 -> 7
 //44 5 78 -> 78
 //22 3 9 -> 22
+Console.WriteLine("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > a) max = b;
+if (c > b) max = c;
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
