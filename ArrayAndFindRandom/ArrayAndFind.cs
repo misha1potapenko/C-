d2214c7b841a8057445170b0125459d07fcb0d0a
misha1potapenko <misha1potapenko@yandex.ru>
2022-07-11 10:10:37 +0300

    add find index array in Rundom

---
+++ ArrayAndFindRandom/ArrayAndFind.cs
@@ -0,0 +1,50 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 100);
+
+        index++;
+    }
+}
+
+void PrintArray(int[] newCol)
+{
+    int count = newCol.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(newCol[position]);
+        position++;
+    }
+}
+
+int IndexOff(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+        
+    }
+    return position;
+}
+
+
+int[] massif = new int[100];
+FillArray(massif);
+PrintArray(massif);
+Console.WriteLine();
+
+
+int pos = IndexOff(massif, 28);
+Console.WriteLine(pos);
