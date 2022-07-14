680f7696c5c6be6375b5fbae45adf11a57efcd3a
misha1potapenko <misha1potapenko@yandex.ru>
2022-07-14 14:43:12 +0300

    added new folder for array and for 3 seminar

--- ArrayAndFindRandom/ArrayAndFind.cs
+++ ArrayAndFindRandom/ArrayAndFind.cs
@@ -5,9 +5,11 @@
     while(index < length)
     {
         collection[index] = new Random().Next(1, 10);
-
+        Console.Write(collection[index]);
+        Console.Write(", ");
         index++;
     }
+    Console.WriteLine();
 //     int count = 0;
 //     int max = collection[count];
 //     int min = collection[count];
@@ -25,58 +27,63 @@
 
     
    
-}  
+ 
     int count = 0;
     int max = collection[count];
     int min = collection[count];
-    while (length < count)
+    while (length > count)
     {
        if(collection[count] > max) max = collection[count]; 
        
        if(collection[count] < min) min = collection[count]; 
-       count +=1;
+       count++;
+      
 
 
+  
     }
-Console.WriteLine(max);
-Console.WriteLine(min);
 
-
-void PrintArray(int[] newCol)
-{
-    int count = newCol.Length;
-    int position = 0;
-    while (position < count)
-    {
-        Console.Write(newCol[position]);
-        position++;
-    }
+    Console.WriteLine($"Максимальное число: {max}");
+    Console.WriteLine($"Минимальное число: {min}");
 }
 
-int IndexOff(int[] collection, int find)
-{
-    int count = collection.Length;
-    int index = 0;
-    int position = -1;
-    while(index < count)
-    {
-        if(collection[index] == find)
-        {
-            position = index;
-            break;
-        }
-        index++;
+
+
+// void PrintArray(int[] newCol)
+// {
+//     int count = newCol.Length;
+//     int position = 0;
+//     while (position < count)
+//     {
+//         Console.Write(newCol[position]);
+//         position++;
+//     }
+// }
+
+// int IndexOff(int[] collection, int find)
+// {
+//     int count = collection.Length;
+//     int index = 0;
+//     int position = -1;
+//     while(index < count)
+//     {
+//         if(collection[index] == find)
+//         {
+//             position = index;
+//             break;
+//         }
+//         index++;
         
-    }
-    return position;
-}
+//     }
+//     return position;
+// }
 
 
 int[] massif = new int[10];
 FillArray(massif);
-PrintArray(massif);
-Console.WriteLine();
+// PrintArray(massif);
+// Console.WriteLine();
 
 
-int pos = IndexOff(massif, 5);
-Console.WriteLine(pos);
+// int pos = IndexOff(massif, 5);
+// Console.WriteLine(pos);
---
+++ TestArray/Program.cs
@@ -0,0 +1,20 @@
+﻿void TestArray (int[] test)
+{
+    int length = test.Length;
+    int count = 0;
+    while (count < length)
+    {
+        test[count] = new Random().Next(1, 10);
+        Console.Write(test[count]);
+        Console.Write (", ");
+        count++;
+
+
+    }
+
+
+
+}
+
+int[] forTest = new int[10];
+TestArray(forTest);
