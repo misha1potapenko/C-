a806f62671f702a2488ba1de9f4d49bc72b60e9e
misha1potapenko <misha1potapenko@yandex.ru>
2022-07-21 08:56:13 +0300

    added reverse number

---
+++ Lecture3/ForToFor/ReversNumber.cs
@@ -0,0 +1,18 @@
+﻿
+int inter = Convert.ToInt32(Console.ReadLine());
+int  a = 0, reverse = 0;
+for (int i = inter; i > 0; i = (i / 10))
+{
+  a = i % 10;
+  reverse = (reverse * 10) + a;
+ Console.Write($"{reverse} ");
+
+}
+// if (reverse == inter) 
+// {
+//     Console.WriteLine("Yes");
+// }
+// else 
+// {
+//     Console.WriteLine("No");
+// }
