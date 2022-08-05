6102130939acdf2b372d8034c4e57d8f26b80d00
misha1potapenko <misha1potapenko@yandex.ru>
2022-08-05 21:09:18 +0300

    added task 3

---
+++ HomeworkSem4Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая создает массив
+// из введенных через запятую цифр
+// и выводит этот массив на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Write("Введите несколько цифр через запятую: ");
+string ForArray = Console.ReadLine()!;
+int length = ForArray.Length;
+
+
+int [] Array = ForArray.Split(',').Select(int.Parse).ToArray();
+
+
+void PrintArray(int[] newCol)
+{
+    int count = newCol.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(newCol[position]);
+        position++;
+    }
+}
+
+PrintArray(Array);
