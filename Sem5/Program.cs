f8db55f046d2025150814f54e1d366e2c175af36
misha1potapenko <misha1potapenko@yandex.ru>
2022-08-05 21:04:19 +0300

    added seminar5

---
+++ Sem5/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 33: Задайте массив. Напишите программу, 
+// которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+Console.WriteLine("Enter number");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] massive = new int[20];
+int sumN = 0;
+int sumP = 0;
+
+for(int i = 0; i < massive.Length; i++)
+    {
+        massive[i] = new Random().Next(-9,10);
+
+        if(massive[i] == number) 
+            {
+                Console.WriteLine("Да");
+            }
+
+       else Console.WriteLine("Нет");;
+
+
+    }
+
+
+
+void PrintArray(int[] col)
+    {
+        int count = col.Length;
+        int position = 0;
+        while (position < count)
+            {
+                Console.Write($"{col[position]} ");
+                position++;
+            }
+    }
+
+PrintArray(massive);
+Console.WriteLine();
+
---
+++ Seminar5/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 31: Задайте массив из 12 элементов, 
+// заполненный случайными числами из промежутка [-9, 9]. 
+// Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] 
+// сумма положительных чисел равна 29, сумма отрицательных равна -20.
+
+Console.WriteLine("Enter the massive size");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] massive = new int[size];
+int sumN = 0;
+int sumP = 0;
+
+for(int i = 0; i < massive.Length; i++)
+    {
+        massive[i] = new Random().Next(-9,10);
+
+        if(massive[i] > 0) 
+            {
+                sumP = sumP + massive[i];
+            }
+
+       else sumN = sumN + massive[i];
+
+
+    }
+
+
+
+void PrintArray(int[] col)
+    {
+        int count = col.Length;
+        int position = 0;
+        while (position < count)
+            {
+                Console.Write($"{col[position]} ");
+                position++;
+            }
+    }
+
+PrintArray(massive);
+Console.WriteLine();
+Console.WriteLine($"Sum of positive numbers is: {sumP}");
+Console.WriteLine($"Sum of negative numbers is: {sumN}");
