1801a2c58d54b718d3add733e1f906a8719c33a4
misha1potapenko <misha1potapenko@yandex.ru>
2022-08-06 17:39:42 +0300

    added solution for task34

--- Homework5/test34/test34.cs
+++ Homework5/test34/test34.cs
@@ -6,24 +6,32 @@
 
 Console.Write("Введете количесво элементов в массиве: ");
 int NumbersOfElements = Convert.ToInt32(Console.ReadLine());
-
 int[] array = new int[NumbersOfElements];
-
 void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
     {
+        array[i] = new Random().Next(111, 999);
+    }
+}
+FillArray(array);
+void PrintArray(int[] newCol)
+    {
+        for (int i = 0; i < newCol.Length; i++)
+            {
+                Console.Write($"{newCol[i]}, ");
+            }
+        Console.WriteLine();
+    }
+PrintArray(array);
+void FindPositive(int[] array)
+    {
+        int count = 0;
         for (int i = 0; i < array.Length; i++)
             {
-                array[i] = new Random().Next(111, 999);
+                if (array[i] % 2 == 0) count++;
+            
             }
+        Console.WriteLine($"Количество положительных элементов: {count}");
     }
-
-FillArray(array);
-
-void PrintArray(int[] newCol)
-{
-   for (int i = 0; i < newCol.Length; i++)
-        {
-            Console.Write($"{newCol[i]}, ");
-        }
-}
-PrintArray(array);
+FindPositive(array);
