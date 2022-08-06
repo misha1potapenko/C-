37bac983a5e3ca7d14f24a1dacd1afcf738480ec
misha1potapenko <misha1potapenko@yandex.ru>
2022-08-06 21:26:24 +0300

    added solution task 38

--- Homework5/test38/task38.cs
+++ Homework5/test38/task38.cs
@@ -13,3 +13,40 @@ void FillArray(int[] array)
                 array[i] = new Random().Next(-40, 41);
             }
     }
+
+void FindMaxMin(int[]array)
+
+    {   
+        int max = array[0];
+        int min = array[0];
+        int maxIndex = 0;
+        int minIndex = 0;
+        int MaxMin = 0;
+        for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                    {
+                        max = array[i];
+                        maxIndex = i;
+                    }
+                else if (array[i] < min)
+                    {
+                        min = array[i];
+                        minIndex = i; 
+                    }
+            }
+        MaxMin = max - min;
+        Console.WriteLine($"min = {min}, max = {max}, minIndex = {minIndex}, maxIndex = {maxIndex} Разница = {MaxMin}");
+    }
+void PrintArray(int[] newCol)
+    {
+        for (int i = 0; i < newCol.Length; i++)
+            {
+                Console.Write($"{newCol[i]}, ");
+            }
+        Console.WriteLine();
+    }
+FillArray(array);
+PrintArray(array);
+FindMaxMin(array);
+
