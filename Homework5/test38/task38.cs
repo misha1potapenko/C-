11ad44b7f98568490a02ea5253a3cdefc9d971b1
misha1potapenko <misha1potapenko@yandex.ru>
2022-08-06 18:42:08 +0300

    added FillArray

---
+++ Homework5/test38/task38.cs
@@ -0,0 +1,15 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между 
+// максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Console.Write("Введите количество элементов в массиве: ");
+int ForArray = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[ForArray];
+
+void FillArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(-40, 41);
+            }
+    }
