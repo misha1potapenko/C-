edd587796923a005cec48721d4fee9e79f06bd53
misha1potapenko <misha1potapenko@yandex.ru>
2022-08-07 22:05:28 +0300

    added matrix

---
+++ lecture4Array/Program.cs
@@ -0,0 +1,22 @@
+﻿// string [,] table = new string [2,5];
+
+// table [1, 2] = "слово";
+
+// for (int rows = 0; rows < 2; rows++)
+// {   
+//     for(int columns =0; columns < 5; columns++)
+//     {
+//          Console.WriteLine($"-{table[rows, columns]}");
+//     }
+
+// }
+
+int[,] matrix = new int[3, 4];
+for (int i = 0; i < 2; i++)
+{   
+    for(int j =0; j < 5; j++)
+    {
+         Console.Write($"-{matrix[i, j]}");
+    }
+    Console.WriteLine();
+}
