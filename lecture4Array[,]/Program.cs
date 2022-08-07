81a042eea6f740f5eacc61b170e96bdfcf0fb8f8
misha1potapenko <misha1potapenko@yandex.ru>
2022-08-07 21:55:40 +0300

    added for in for

--- lecture4Array[,]/Program.cs
+++ lecture4Array[,]/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("2Hello, World!");
+﻿string [,] table = new string [2,5];
+
+table [1, 2] = "слово";
+
+for (int rows = 0; rows < 2; rows++)
+{   
+    for(int columns =0; columns < 5; columns++)
+    {
+         Console.WriteLine($"-{table[rows, columns]}");
+    }
+   
+}
