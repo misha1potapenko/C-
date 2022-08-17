ff7d374c6bb1a547956b4017894e15e22b775f59
misha1potapenko <misha1potapenko@yandex.ru>
2022-08-17 19:45:49 +0300

    added modified file

--- Examples002/Program.cs
+++ Examples002/Program.cs
@@ -2,3 +2,26 @@
 string username = Console.ReadLine()!;
 Console.WriteLine("Привет, ");
 Console.WriteLine(username);
+
+// string [,] table = new string [2,5];
+
+// table [1, 2] = "слово";
+
+// for (int rows = 0; rows < 2; rows++)
+// {   
+//     for(int columns =0; columns < 5; columns++)
+//     {
+//          Console.WriteLine($"-{table[rows, columns]}");
+//     }
+
+// }
+
+int[,] matrix = new int[3, 4];
+for (int i = 0; i < 3; i++)
+{   
+    for(int j =0; j < 4; j++)
+    {
+         Console.Write($"-{matrix[i, j]}");
+    }
+    Console.WriteLine();
+}
--- lecture4Array/Program.cs
+++ lecture4Array/Program.cs
@@ -12,9 +12,9 @@
 // }
 
 int[,] matrix = new int[3, 4];
-for (int i = 0; i < 2; i++)
+for (int i = 0; i < 3; i++)
 {   
-    for(int j =0; j < 5; j++)
+    for(int j =0; j < 4; j++)
     {
          Console.Write($"-{matrix[i, j]}");
     }
