2b9d1806dae8873896a6e68bc46bcba282001a53
misha1potapenko <misha1potapenko@yandex.ru>
2022-08-22 20:11:10 +0300

    added solution task66

--- Hw9/task64/Program.cs
+++ Hw9/task64/Program.cs
@@ -14,25 +14,5 @@ string Numbers(int M, int N)
         if (M <= N) return $"{M}, " + Numbers(M+1, N);
         else return string.Empty;
     }
-// void FillArray(int[,] array)
-//     {
-//         for(int i = 0; i < array.GetLength(0); i++)
-//             {
-//                 for(int j = 0; j < array.GetLength(1); j++)
-//                     {
-//                         array[i,j] = new Random().Next(1, 20);
-//                     }
-//             }
-//     }
-// void PrintArray(int[,] array)
-//     {
-//         for(int i = 0; i < array.GetLength(0); i++)
-//             {
-//                 for(int j = 0; j < array.GetLength(1); j++)
-//                     {
-//                         Console.Write($"{array[i,j]} ");
-//                     }
-//                 Console.WriteLine();
-//             }
-//     }
+
 Console.WriteLine(Numbers(M,N));
---
+++ Hw9/task66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N.
+//  Напишите программу, которая найдёт сумму 
+//  натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+Console.Write("Введите M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int Numbers(int M, int N)
+
+    {
+        
+        if (M <= N) return M + Numbers(M+1, N);
+        else return 0;
+
+    }
+Console.WriteLine(Numbers(M,N));
