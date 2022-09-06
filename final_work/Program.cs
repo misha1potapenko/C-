5e84027011cde299cb9a84a0483b03815110b33e
misha1potapenko <misha1potapenko@yandex.ru>
2022-09-06 20:12:43 +0300

    added block diagram of the algorithm

--- final_work/Program.cs
+++ final_work/Program.cs
@@ -1,2 +1,23 @@
 ﻿// Написать программу, которая из имеющегося массива сток
 // формирует массив из строк, длина которых меньше либо равна 3 символа.
+string[] firstArray = {"sd1", "awe", "dsf", "s45d", "aw555e", "d5f"};
+string[] secondArray = new string[firstArray.Length];
+
+for (int i = 0; i < firstArray.Length; i++)
+    {  
+        if (Convert.ToInt32(firstArray[i].Length) < 4) secondArray[i] = firstArray[i]; 
+    }
+
+void PrintArray(string[] col)
+    {
+        int count = col.Length;
+        int position = 0;
+        while (position < count)
+            {
+                Console.Write($"{col[position]} ");
+                position++;
+            }
+    }
+
+PrintArray(secondArray);
+
